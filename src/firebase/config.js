7051b4e056511b3d3b2dd625d5d0577218b2ba3e
Randika Rodrigo <subash.rodrigo@gmail.com>
2021-05-02 21:18:40 +0530

    Initial commit with redux setup and Home page comonents including project structure with firebase config setup.

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyDMA6d92Ib00vuVbKOET0MXtNodJdyXL6g',
+	authDomain: 'netflix-clone-2c3d5.firebaseapp.com',
+	projectId: 'netflix-clone-2c3d5',
+	storageBucket: 'netflix-clone-2c3d5.appspot.com',
+	messagingSenderId: '480270955815',
+	appId: '1:480270955815:web:eeb12b928336d4fc946502'
+};
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.styles.css';
+
+const Home = () => {
+	return (
+		<div className='home'>
+			{/* Navbar */}
+
+			{/* Banner */}
+
+			{/* Rows */}
+		</div>
+	);
+};
+
+export default Home;
